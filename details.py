8c00698ef1fc23e476c5ef3fe5da59a1fea49d2e
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-02-16 21:47:05 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,6 +1,6 @@
 api_id = 24508138
 api_hash = "94bf6b17836bedb54c4c3ab0aaf26eaf"
-bot_token = "6594535402:AAHMX-9zO4JIVwe7_DLbrHNyJ0VC87ibflM"
+bot_token = "6395070972:AAGMIeSKtwZPHnQtszsxQlg3puuo7cGGr9o"
 auth_users = [6585878012]
 sudo_user = -1001915281770
 log_channel = -1001915281770
