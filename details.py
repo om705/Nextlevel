3804d0c6e8eca317a28b863f69e11888a390bf1d
VISHNU <151006988+OMlinks@users.noreply.github.com>
2024-02-16 21:50:50 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
 api_id = 24508138
 api_hash = "94bf6b17836bedb54c4c3ab0aaf26eaf"
 bot_token = "6395070972:AAGMIeSKtwZPHnQtszsxQlg3puuo7cGGr9o"
-auth_users = [6585878012]
-sudo_user = -1001915281770
-log_channel = -1001915281770
-txt_channel = -1001915281770
+auth_users = [5829511291]
+sudo_user = 5033114900
+log_channel = -1002032653593
+txt_channel = -1002032653593
