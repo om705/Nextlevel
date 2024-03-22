c203a75a4e0c9c15801435dce5f556070e10fe2d
om705 <61368509+om705@users.noreply.github.com>
2024-03-22 11:33:39 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
-api_id = 24508138
-api_hash = "94bf6b17836bedb54c4c3ab0aaf26eaf"
-bot_token = "6395070972:AAGMIeSKtwZPHnQtszsxQlg3puuo7cGGr9o"
-auth_users = [5829511291]
-sudo_user = 5033114900
-log_channel = -1002032653593
-txt_channel = -1002032653593
+api_id = 26115463
+api_hash = "054557eca362bba15769130a5fadf0de"
+bot_token = "6873302602:AAG0x-n_9zqfW7qN3QqczcMA91YXP9cEC4o"
+auth_users = [6631088681]
+sudo_user = 6631088681
+log_channel = -1002141059330
+txt_channel = -1002070220820
